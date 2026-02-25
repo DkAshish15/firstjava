1676aede6e08453c68c7997516d3f6919b98c3aa
DkAshish15 <ashishdhakne2006@gmail.com>
2026-02-25 11:55:20 +0530

    Replace HelloWorld with DataTypesExample class

--- first.java
+++ first.java
@@ -1,7 +1,9 @@
-public class HelloWorld {
-    public static void main(String[] args
-        System.out.println("Hello, Ashish World!");
+public class DataTypesExample {
+    public static void main(String[] args) {
+        int number = 10;
+        double price = 19.99;
+        boolean isTrue = true;
+        // Variables hold different data types
+        System.out.println("Number: " + number + ", Price: " + price);
     }
 }
-
-

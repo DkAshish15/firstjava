b8ab3193855d3681c7bcc925b563f6d3d1691f43
DkAshish15 <ashishdhakne2006@gmail.com>
2026-02-26 11:15:01 +0530

    Fix syntax errors in DataTypesExample class
    Fixed syntax error by removing extra braces and added missing semicolon.

--- first.java
+++ first.java
@@ -1,10 +1,9 @@
 public class DataTypesExample {
     public static void main(String[] args) {
         int number = 10;
-        double price = 19.99;
+        double price = 19.99
         boolean isTrue = true;
         // Variables hold different data types
         System.out.println("Number: " + number + ", Price: " + price);
     }
-} }
-
+} 
